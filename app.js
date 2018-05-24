e59537a34e4b6f728fa5767cddfdc8cd8313c4c2
kristelseymus <39592724+kristelseymus@users.noreply.github.com>
2018-05-24 16:32:29 +0200

    database string

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ moment.locale('nl');
 //mongodb://localhost/ptlab
 //mongodb://admin:admin@ds251988.mlab.com:51988/ptlab
 /* Connect to mongo database */
-mongoose.connect('mongodb://localhost/ptlab', {
+mongoose.connect('mongodb://admin:admin@ds251988.mlab.com:51988/ptlab', {
    useMongoClient: true
  }, function (err, db) {
    if (!err) {
